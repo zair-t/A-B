925612b84078d485ff005042c5002c8221da6355
zair-t <71732403+zair-t@users.noreply.github.com>
2020-12-09 09:29:12 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,53 +1,42 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    TextView textt;
-    Button sum;
-    EditText ed1, ed2, ed3;
+    TextView text;
+    Button sum,minus;
+    EditText ed1,ed2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sum = findViewById(R.id.sum);
-        textt = findViewById(R.id.textt);
-        ed1 = findViewById(R.id.num1);
-        ed2 = findViewById(R.id.num2);
-        ed3 = findViewById(R.id.num3);
+        this.sum = findViewById(R.id.sum);
+        this.minus = findViewById(R.id.minus);
+        this.text = findViewById(R.id.text);
+        this.ed1 = findViewById(R.id.num1);
+        this.ed2 = findViewById(R.id.num2);
         sum.setOnClickListener(this);
+        minus.setOnClickListener(this);
     }
-
 
     @Override
     public void onClick(View v) {
         String t1 = ed1.getText().toString();
         String t2 = ed2.getText().toString();
-        String t3 = ed3.getText().toString();
-        double a = Double.parseDouble(t1);
-        double b = Double.parseDouble(t2);
-        double c = Double.parseDouble(t3);
-        if (a == 0){
-            textt.setText("error");
+        int a = Integer.parseInt(t1);
+        int b = Integer.parseInt(t2);
+        if(v.getId() == R.id.sum) {
+            a += b;
+        } else {
+            a -= b;
         }
-        double D = b*b - 4*a*c;
-        if (D < 0){
-            textt.setText("error");
-        }
-        double x1 = ((-b+Math.sqrt(D))/(2*a));
-        double x2 = ((-b-Math.sqrt(D))/(2*a));
-        if (D == 0){
-            textt.setText(String.valueOf(x1));
-        }
-        else if (D > 0){
-            textt.setText(String.format("%.2f %.2f",x1, x2));
-        }
+        text.setText(String.valueOf(a));
     }
 }
